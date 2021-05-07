79de55391bb56839b7997c80970018844a15f2a1
Olayinka Ibrahim <olayinka.ibrahim@etranzact.com>
2021-05-07 23:41:41 +0100

    fixed server side validationerror for bestSeller and hotSale

--- src/service/product.service.js
+++ src/service/product.service.js
@@ -18,8 +18,34 @@ module.exports = {
                   message: 'Product Description is required.',
                 };
             }),
+            bestSeller:Joi.bool().required().valid([true,false]),
+            hotSale:Joi.number().required().when('bestSeller',{
+              is:true,
+              then:Joi.number().required().valid([0]).error((errors) => {
+                return errors.map(error => {
+                  switch (error.type) {
+                    case "number.base":
+                      return { message: "hotSale must be a number of value 0, when bestSeller is true" };
+                    case "any.allowOnly":
+                      return { message: "hotSale must be of value 0, if bestSeller is true" };
+                  }
+                }
+                )
+              }),
+              otherwise:Joi.number().valid([0,1]).error((errors) => {
+                return errors.map(error => {
+                  switch (error.type) {
+                    case "number.base":
+                      return { message: "hotSale must be a number" };
+                    case "any.allowOnly":
+                      return { message: "hotSale must be of value 0 or 1" };
+                  }
+                }
+                )
+              })
+            }),
             productPrice: Joi.number().required(),
-            category: Joi.array().required(),
+            category: Joi.array().required().min(1),
         })
         const { value, error } = Joi.validate(requestBody, schema,{ abortEarly: false ,allowUnknown: true });
         if (error && error.details) {
