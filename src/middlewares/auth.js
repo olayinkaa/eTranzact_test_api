63c1e9adb150e65448c594f298b53a4f41f659bc
Ibrahim Olayinka <ibrahimolayinkaa@gmail.com>
2021-05-12 10:54:28 +0100

    auth.js

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -16,7 +16,7 @@ const auth = (req, res, next) => {
     const decoded = jwt.verify(token, process.env.SECRET);
     req.user = decoded;
     next();
- 
+
   } catch (err) {
     res.status(401).json({ message: 'Token is not valid' });
   }
